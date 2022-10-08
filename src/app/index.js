025f343204998ec0da8f2bc3de05818531c14f64
Rudolf Schmidt <me@rudionrails.com>
2022-10-08 15:35:25 +0200

    feat(blockchain-pow-example): Adds POST /mine endpoint

--- src/app/index.js
+++ src/app/index.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { createBlockchain } = require("../blockchain");
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
-const app = express();
 const blockchain = createBlockchain();
+const app = express();
+
+app.use(bodyParser.json());
 
 app.get("/blocks", (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post("/mine", (req, res) => {
+  const block = blockchain.addBlock(req.body.data);
+  console.log("New block added:", block);
+
+  res.status(201).send("Block created");
 });
 
 app.listen(HTTP_PORT, () => {
